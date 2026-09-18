f2d246f24cad0f24b647c099537f4cfbd9ed9a65
yash agrawal <yashmtr1744@gmail.com>
2026-09-18 14:24:25 +0530

    fix: allow cross-site session cookie

--- backend/main.py
+++ backend/main.py
@@ -48,10 +48,10 @@ app.add_middleware(
 
 # Session Middleware for Authlib OAuth
 app.add_middleware(
-    SessionMiddleware, 
+    SessionMiddleware,
     secret_key=os.getenv("SECRET_KEY") or os.urandom(24).hex(),
     https_only=True,
-    same_site='lax'
+    same_site='none'
 )
 
 oauth = OAuth()
